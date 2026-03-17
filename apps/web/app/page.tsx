9ef01d86151a3fa75f920856dd7ff83ef9c50247
Abhishek <abhiskul00@gmail.com>
2026-03-17 20:46:04 +0530

    add clerk auth

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,33 @@
 'use client'
 
-import { useQuery } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@workspace/ui/components/Button";
+import { useQuery, useMutation } from "convex/react";
 import { api } from "@workspace/backend/convex/_generated/api"
+import { Authenticated, Unauthenticated } from "convex/react";
+
 
 export default function Page() {
   const users = useQuery(api.users.getMany)
+  const addUser = useMutation(api.users.add)
+
   return (
-    <div>
-      {JSON.stringify(users)}
-    </div>
+    <>
+      <Authenticated>
+        <div>
+          <p>app</p>
+          <Button onClick={() => addUser()}>addUser</Button>
+          <UserButton />
+          <div>
+            {JSON.stringify(users, null, 2)}
+          </div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>unauthenticated must sign </p>
+        <SignInButton />
+      </Unauthenticated>
+    </>
+
   )
 }
--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -1,9 +1,31 @@
-import { query } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const getMany = query({
     args: {},
     handler: async (ctx) => {
         const users = await ctx.db.query("users").collect();
+
+        return users;
+
+    },
+});
+
+export const add = mutation({
+    args: {},
+
+    handler: async (ctx) => {
+
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (identity == null) {
+            throw new Error("unauthorized");
+
+        }
+        const users = await ctx.db.insert("users", {
+            name: "John Doe",
+            tokenIdentifier: "1234567890",
+        });
 
         return users;
 
